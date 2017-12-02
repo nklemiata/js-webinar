b52ca2b562b445a1fab0654f31f083810723a61d
NataNout\Ната <nata.klemiato@gmail.com>
2017-12-03 00:30:16 +0200

    Commit Module 1 task

--- module-1/fibonacci.js
+++ module-1/fibonacci.js
@@ -4,7 +4,7 @@
  * The function returns the nth value of
  * the Fibonacci sequence
  *
- * @param {number} n (n >= 0)
+ * @param {number} n (n >= 1)
  * @returns {number}
  * @throws {Error} Fibonacci number or 0 if any arguments are not proper
  */
@@ -19,7 +19,35 @@ function fibonacci(n) {
      */
     // PLACE YOUR CODE BETWEEN THIS...
 
+    // -------------- Varian 1 ----------------------------------
+    if (n<0) {
+        return nThFibonacci=0;
+    }
+    else if (n<2) {
+        return nThFibonacci=n;
+    } else {
+        n=fibonacci(n-1) + fibonacci(n-2)
+        }
+        nThFibonacci=n;
 
+
+/*
+    //------- Varian 2 - without recursion ------
+    if (n<0) {
+        return nThFibonacci=0;
+    }
+    else if (n<2) {
+        return nThFibonacci=n;
+    } else {
+        var a = 1, b = 1;
+        for (var i = 3; i <= n; i++) {
+            var c = a + b;
+            a = b;
+            b = c;
+        }
+        return nThFibonacci=b;
+    }
+*/
     // ...AND THIS COMMENT LINE!
     return nThFibonacci;
 }
