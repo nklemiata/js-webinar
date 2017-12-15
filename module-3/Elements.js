677ee6bbeb9ab666d57179c4ad3b2fb634662924
Nata Klemiata <nata.klemiato@gmail.com>
2017-12-15 04:10:54 +0200

    Module 3 - Elements.js updated

--- module-3/Elements.js
+++ module-3/Elements.js
@@ -1,1 +1,27 @@
 'use strict';
+
+class Elements {
+    constructor(name, locator) {
+        this.name = name;
+        this.locator = locator;
+        this.parent = null;
+        this.children = null;
+    }
+
+    setParent(parent) {
+        this.parent = parent;
+    }
+
+   all(name) {
+        if (!name) {
+            return element.all(this.locator);
+        }
+        throw new Error('There is no parent like: ' + name);
+    }
+
+    get(x) {
+        return x   //Did we have to use smth like "return Object.values(this.Elements);" ? 
+    }
+}
+
+    module.exports = Elements;
