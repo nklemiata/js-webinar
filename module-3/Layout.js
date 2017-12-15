6ae3f1371f86b241d7aa3bce3999e6ed449850d2
Nata Klemiata <nata.klemiato@gmail.com>
2017-12-15 04:49:26 +0200

    Part of Layout.js solution

--- module-3/Layout.js
+++ module-3/Layout.js
@@ -1,1 +1,50 @@
 'use strict';
+
+class Layout {
+    constructor(name, url, locator) {
+        this.name = name;
+        this.url = url;
+        this.locator = locator;
+        this.parent = null;
+        this.children = {};
+    }
+
+    setParent(parent){
+        this.parent = parent;
+        if (setParent != null){
+            throw new Error('The parent should not be set: ' + parent.name);
+        };
+    }
+
+    addChildren(child){     //have to connect to Element somehow
+        if (this.children[child.name]) {
+            throw new Error('The child has been already added: ' + child.name);
+        }
+        this.children[child.name] = child;
+        child.setParent(this);
+    }
+
+    get(name) {
+        if (!name) {
+            return element(this.locator);
+        }
+        if (this.children[name]) {
+            return this.children[name].get();
+        }
+        throw new Error('There is no child like: ' + name);
+    }
+
+    load(url){
+        browser.get(url);
+    }
+    
+
+
+
+
+
+
+}
+
+
+module.exports = Layout;
