707e9f7eb6598089d6995c72434ebcb994fed662
MINSK\Natallia_Klemiata <Natallia_Klemiata@epam.com>
2017-12-21 18:55:33 +0300

    Fixed 'toCamelCase.js'

--- module-4/toCamelCase.js
+++ module-4/toCamelCase.js
@@ -1,5 +1,5 @@
 'use strict';
-const CHARACTERS_TO_KEEP = '0123456789qwertuiopasdfghjklzyxcvbnm';
+const CHARACTERS_TO_KEEP = "0123456789qwertzuiopasdfghjklyxcvbnm";
 /**
  * It returns the camel-case version of string.
  * E.g.: simple lowercase string => SimpleLowercaseString
@@ -10,22 +10,20 @@ const CHARACTERS_TO_KEEP = '0123456789qwertuiopasdfghjklzyxcvbnm';
 function toCamelCase(toConvert) {
     if (typeof toConvert !== 'string') {
         return '';
+     /*   const tmp = toConvert.split(' ');
+        console.log(JSON.stringify(tmp));
+        return tmp.join('');*/
     }
 
-    //log
-    const tmp = toConvert.split(' ');
-    console.log(JSON.stringify(tmp));
-    return tmp.join('');
 
     return toConvert
         .split(' ')
         .map(word => word
             .split('')
-            .filter(char => CHARACTERS_TO_KEEP.indexOf(char.toLowerCase()) !== -1)  //Returns -1 if the item is not found.
+            .filter(char => CHARACTERS_TO_KEEP.indexOf(char.toLocaleLowerCase()) !== -1)
             .join('')
         )
-        .map(word => word.charAt(0).toUpperCase() + word.substr(1).toLowerCase())
+        .map(word => word.charAt(0).toLocaleUpperCase() + word.substr(1).toLocaleLowerCase())
         .join('');
-    console.log(JSON.stringify(word));
 }
 module.exports = toCamelCase;
